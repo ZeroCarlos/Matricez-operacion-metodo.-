deeeee2f778e519e4b14a758367b2cb8c264abed
Carlos R <ZeroCarlos@users.noreply.github.com>
2015-10-17 19:45:04 -0500

    Update fun_datos.h

--- fun_datos.h
+++ fun_datos.h
@@ -10,7 +10,7 @@ void ing_igc(int *num, float Sist[][100]);
 void imp_sis(int Dim, float Sist[][100]);
 void operador_metodo_gauss(int Dim, float Sist[][100]);
 
-int i, j, f, c, a, b, cant;
+//int i, j, f, c, a, b, cant;
 //int val[max][max];
 /*int cantidad;
 int g_cont, g_cont1;
