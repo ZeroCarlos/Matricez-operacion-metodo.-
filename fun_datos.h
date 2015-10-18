1d18b2cb324c175bb28ef9672f9184386fc485bb
Carlos R <ZeroCarlos@users.noreply.github.com>
2015-10-17 19:20:42 -0500

    Create fun_datos.h

---
+++ fun_datos.h
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <malloc.h>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void ing_igc(int *num, float Sist[][100]);
+void imp_sis(int Dim, float Sist[][100]);
+void operador_metodo_gauss(int Dim, float Sist[][100]);
+
+int i, j, f, c, a, b, cant;
+//int val[max][max];
+/*int cantidad;
+int g_cont, g_cont1;
+int ecuaciones[100][100];*/
